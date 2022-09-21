f7b43b423449fc9473cd5d38cb189849d74652f0
Julia <juliakaufmann@live.se>
2022-09-22 00:05:37 +0200

    Try a solution for the balls

--- Model.java
+++ Model.java
@@ -15,8 +15,8 @@ class Model {
 
 	double[] collision_vector(Ball b1, Ball b2){
 		double collision_vs[] = new double[2];
-		double vx = (balls[0].x - balls[1].x);
-		double vy = (balls[0].y) - (balls[1].y);
+		double vx = (b1.x - b2.x);
+		double vy = (b1.y) - (b2.y);
 		collision_vs[0] = vx;
 		collision_vs[1] = vy;
 		return collision_vs;
@@ -88,12 +88,15 @@ class Model {
 		
 		// Initialize the model with a few balls
 		balls = new Ball[2];
-		balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2);
-		balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3);
+		balls[0] = new Ball(width / 3, height * 0.9, 0.9, 3.6, 0.2, 1000);
+		balls[1] = new Ball(2 * width / 3, height * 0.7, -1.3, 1.6, 0.3, 10);
 	}
 
 	void step(double deltaT) {
 		// TODO this method implements one step of simulation with a step deltaT
+		if(Math.sqrt(Math.pow((balls[0].x - balls[1].x), 2) + Math.pow((balls[0].y) - (balls[1].y), 2)) <= balls[0].radius + balls[1].radius + 0.002){
+			collison2(balls[0], balls[1]);
+		}
 		for (Ball b : balls) {
 			// detect collision with the border
 			if (b.x < b.radius || b.x > areaWidth - b.radius) {
@@ -102,33 +105,73 @@ class Model {
 			if (b.y < b.radius || b.y > areaHeight - b.radius) {
 				b.vy *= -1;
 			}
-			
+
+			// add gravity to our balls
+			b.vy -= deltaT * 9.82;
+
 			// compute new position according to the speed of the ball
 			b.x += deltaT * b.vx;
 			b.y += deltaT * b.vy;
-			b.vy -= deltaT * 9.82;
-		}
-		if(Math.sqrt(Math.pow((balls[0].x - balls[1].x), 2) + Math.pow((balls[0].y) - (balls[1].y), 2)) <= balls[0].radius + balls[1].radius){
-			collison(balls[0], balls[1]);
 		}
 	}
-	
+
+	private void collison2(Ball ball1, Ball ball2) {
+		//Collision vector that we turn in to the new base-vector for x', y'.
+		double[] xPrimBasisVector = collision_vector(ball1, ball2);
+		double[] yPrimBasisVector = new double[]{0,0};
+		yPrimBasisVector[0] = -xPrimBasisVector[1];
+		yPrimBasisVector[1] = xPrimBasisVector[0];
+
+
+		//Transform old speed-vectors to new coordinate system with invers. We calculate A^-1 below:
+		double det = 1/(xPrimBasisVector[0]*yPrimBasisVector[1] - (yPrimBasisVector[0]*xPrimBasisVector[1]));
+		double[] xPrimInvers = new double[]{det*(yPrimBasisVector[1]), det*(-xPrimBasisVector[1])};
+		double[] yPrimInvers = new double[]{det*(-yPrimBasisVector[0]), det*(xPrimBasisVector[0])};
+
+		//Transform old speed-vectors to new coordinate system:
+		double[] ball1VelocityTransformed = new double[]{xPrimInvers[0]*ball1.vx + yPrimInvers[0]*ball1.vy, xPrimInvers[1]*ball1.vx + yPrimInvers[1]*ball1.vy};
+		double[] ball2VelocityTransformed = new double[]{xPrimInvers[0]*ball2.vx + yPrimInvers[0]*ball2.vy, xPrimInvers[1]*ball2.vx + yPrimInvers[1]*ball2.vy};
+
+
+		//Calculate the new speeds in the new coordinates - 2 linear equations:
+		double ball1XVelocity = (ball1.mass*ball1VelocityTransformed[0] + ball2.mass*ball2VelocityTransformed[0] - ball2.mass*(ball1VelocityTransformed[0]- ball2VelocityTransformed[0]))/(ball2.mass + ball1.mass);
+		double ball2XVelocity = (ball1VelocityTransformed[0] - ball2VelocityTransformed[0] + ball1XVelocity);
+
+		ball1VelocityTransformed[0] = ball1XVelocity;
+		ball2VelocityTransformed[0] = ball2XVelocity;
+		//ball1VelocityTransformed[1] *= -1;
+		//ball2VelocityTransformed[1] *= -1;
+
+
+		//Rotate back the new speed vectors:
+		double[] ball1VelocityReturned = new double[]{xPrimBasisVector[0]*ball1VelocityTransformed[0] + yPrimBasisVector[0]*ball1VelocityTransformed[1], xPrimBasisVector[1]*ball1VelocityTransformed[0] + yPrimBasisVector[1]*ball1VelocityTransformed[1]};
+		double[] ball2VelocityReturned = new double[]{xPrimBasisVector[0]*ball2VelocityTransformed[0] + yPrimBasisVector[0]*ball2VelocityTransformed[1], xPrimBasisVector[1]*ball2VelocityTransformed[0] + yPrimBasisVector[1]*ball2VelocityTransformed[1]};
+
+
+		//Giv our balls the new speeds:
+		ball1.vx = ball1VelocityReturned[0];
+		ball1.vy = ball1VelocityReturned[1];
+		ball2.vx = ball2VelocityReturned[0];
+		ball2.vy = ball2VelocityReturned[1];
+	}
+
 	/**
 	 * Simple inner class describing balls.
 	 */
 	class Ball {
 		
-		Ball(double x, double y, double vx, double vy, double r) {
+		Ball(double x, double y, double vx, double vy, double r, double mass) {
 			this.x = x;
 			this.y = y;
 			this.vx = vx;
 			this.vy = vy;
 			this.radius = r;
+			this.mass = mass;
 		}
 
 		/**
 		 * Position, speed, and radius of the ball. You may wish to add other attributes.
 		 */
-		double x, y, vx, vy, radius;
+		double x, y, vx, vy, radius, mass;
 	}
 }
